34b10a7c51ce504f7501b83abbcc0d7c06b28888
wangye707 <1119744330@qq.com>
2019-04-02 16:44:51 +0800

    test debug

---
+++ Test/debug.py
@@ -0,0 +1,14 @@
+#!D:/workplace/python
+# -*- coding: utf-8 -*-
+# @File  : debug.py
+# @Author: WangYe
+# @Date  : 2019/4/2
+# @Software: PyCharm
+a = 1
+b = 10
+c = 0
+a = a + 1
+b = b + 10
+c = b / c
+
+print(c)
